652b1faab8bc93e9a6df4c9de570d45e47925121
floh-bhaptics <floh@fahrenbergers.de>
2022-07-25 17:35:40 +0200

    Add project files.

---
+++ Vertigo2_bhaptics.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MelonLoader;
+using HarmonyLib;
+using MyBhapticsTactsuit;
+
+namespace Vertigo2_bhaptics
+{
+    public class Vertigo2_bhaptics : MelonMod
+    {
+        public static TactsuitVR tactsuitVr;
+
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+            tactsuitVr = new TactsuitVR();
+            tactsuitVr.PlaybackHaptics("HeartBeat");
+        }
+        /*
+        [HarmonyPatch(typeof(VertigoPlayer), "Die", new Type[] { })]
+        public class bhaptics_PlayerDies
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                tactsuitVr.StopThreads();
+            }
+        }
+        */
+    }
+}
